62868153dfcd5e710872c4acadf39630217fcbcc
helton <heltona687@gmail.com>
2021-12-17 14:59:45 -0300

    done

--- App/assets/dashboard.php
+++ App/assets/dashboard.php
@@ -43,7 +43,7 @@
       <h1 class="title">Dashboard</h1>
     </div>
     <div class="infor">
-      You have 4 products added on this store: <a href="/product/create" class="btn-action">Add new Product</a>
+      You have <?= count($prods) ?> products added on this store: <a href="/product/create" class="btn-action">Add new Product</a>
     </div>
     <ul class="product-list">
     <!-- products - -->
